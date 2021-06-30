08f520809e0d147e6d8e550af2a6d94765698e77
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-06-30 16:06:58 -0400

    Fix user-agent test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { buildUserAgentHeader } from "."
 
 test('buildUserAgentHeader', () => {
-  expect(buildUserAgentHeader({ vendor: 'vendor', product: 'product', build: 'build'})).toMatch('Integration/1.0 (vendor; product; Build/build) @jupiterone/tenable-client-nodejs/0.0.0 (@jupiterone/tenable-client-nodejs/');
+  expect(buildUserAgentHeader({ vendor: 'vendor', product: 'product', build: 'build'})).toMatch('Integration/1.0 (vendor; product; Build/build) @jupiterone/tenable-client-nodejs/');
 });
